20a148d734bb87e98707211f46d6d8d2c95a0ef4
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-01-28 19:09:38 -0300

    login

--- webscraping/app.py
+++ webscraping/app.py
@@ -19,10 +19,11 @@ async def get_skills(link):
         await page.fill("input#base-sign-in-modal_session_key", f"{email}")
         await page.fill("input#base-sign-in-modal_session_password", f"{senha}")
         
-        await page.click("button:text('Entrar')[type='submit']")
+        await page.press("input#base-sign-in-modal_session_password", "Enter")
         
         await asyncio.sleep(10)
         
         await browser.close()
         
 asyncio.run(get_skills("https://www.linkedin.com/jobs/search/?currentJobId=4114551949&distance=25&geoId=106057199&keywords=python&origin=JOBS_HOME_SEARCH_CARDS"))
+
