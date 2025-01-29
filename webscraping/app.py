e2443060cf57d8f3b016a3b8ffd09a6027a73228
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-01-29 07:34:28 -0300

    login upgrate
    melhora no sistema de login

--- webscraping/app.py
+++ webscraping/app.py
@@ -5,25 +5,31 @@ from playwright.async_api import async_playwright
 email = os.getenv("email")
 senha = os.getenv("senha")
 
-
-async def get_skills(link):
+async def get_skills(link, keyword):
     async with async_playwright() as play:
         browser = await play.chromium.launch(headless=False)
         page = await browser.new_page()
         
         await page.goto(link)
         
-        await page.wait_for_selector("button[data-modal='base-sign-in-modal']", timeout=5000)
-        await page.click("button[data-modal='base-sign-in-modal']")
+        # await page.wait_for_selector("button[data-modal='base-sign-in-modal']", timeout=5000)
+        # await page.click("button[data-modal='base-sign-in-modal']")
         
-        await page.fill("input#base-sign-in-modal_session_key", f"{email}")
-        await page.fill("input#base-sign-in-modal_session_password", f"{senha}")
+        await page.fill("input#session_key", f"{email}")
+        await page.fill("input#session_password", f"{senha}")
         
-        await page.press("input#base-sign-in-modal_session_password", "Enter")
+        await page.press("input#session_password", "Enter")
+        
+        await page.wait_for_selector("input[id^=jobs-search-box-keyword-id]")
+        
+        await page.fill("input[id^=jobs-search-box-keyword-id]",
+                        keyword)
+        
+        await page.press("input[id^=jobs-search-box-keyword-id]", "Enter")
         
         await asyncio.sleep(10)
         
         await browser.close()
         
-asyncio.run(get_skills("https://www.linkedin.com/jobs/search/?currentJobId=4114551949&distance=25&geoId=106057199&keywords=python&origin=JOBS_HOME_SEARCH_CARDS"))
+asyncio.run(get_skills("https://www.linkedin.com/jobs/", "Javascript"))
 
